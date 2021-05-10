7f1ee78e15eaeddedab3ef997bc44672b8c5ff20
jorge <georgellanque@gmail.com>
2021-05-10 18:18:47 -0400

    Adding icons

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import { brown } from "@material-ui/core/colors";
+import { purple } from "@material-ui/core/colors";
+import Layout from './components/Layout';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#fefefe"
+      main: "#000"
     },
-    secondary: brown, 
+    secondary: purple, 
   },
   typography: {
     fontFamily: 'Quicksand',
@@ -24,14 +25,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <Notes />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <Notes />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
---
+++ src/components/NoteCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { DeleteOutlined } from "@material-ui/icons";
+
+
+const useStyles = makeStyles({
+    test: {
+        border: (note)=>{
+            if(note.category == 'work'){
+                return '1px solid red'
+            }
+        }
+    }
+})
+
+export default function NoteCard({note, handleDelete}) {
+    const classes = useStyles(note)
+
+    return (
+        <div>
+            <Card elevation={1} className={classes.test} >
+                <CardHeader
+                 action={
+                     <IconButton onClick={() => handleDelete(note.id) } >
+                         <DeleteOutlined />
+                     </IconButton>
+                 }
+                 title={note.title}
+                 subheader={note.category}
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" >
+                        {note.details}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
