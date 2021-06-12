d36eda0c56a89a445119a881f4ba7d93d409e277
jorge <georgellanque@gmail.com>
2021-06-12 15:26:12 -0400

    Add new styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Notes from "./pages/Notes";
+import Create from "./pages/Create";
+import Dummy from "./pages/Dummy";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import { purple } from "@material-ui/core/colors";
-import Layout from './components/Layout';
+import Layout from "./components/Layout";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#fefefe"
+      main: "#fefefe",
     },
-    secondary: purple, 
+    secondary: purple,
   },
   typography: {
-    fontFamily: 'Quicksand',
+    fontFamily: "Quicksand",
     fontWeightLight: 400,
     fontWeightRegular: 500,
     fontWeightMedium: 600,
-    fontWeightBold: 700
-  }
-})
+    fontWeightBold: 700,
+  },
+});
 
 function App() {
   return (
@@ -32,6 +33,9 @@ function App() {
             </Route>
             <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/dummy">
+              <Dummy />
             </Route>
           </Switch>
         </Layout>
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import {FormControlLabel, makeStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { FormControlLabel, makeStyles } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
 import { useHistory } from "react-router-dom";
-import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
 const useStyles = makeStyles({
   field: {
     marginTop: 20,
     marginBottom: 20,
-    display: 'block'
-  }
+    display: "block",
+  },
 
   // btn: {
   //   fontSize: 60,
@@ -29,55 +29,51 @@ const useStyles = makeStyles({
   //   textDecoration: 'underline',
   //   marginBottom: 20
   // }
-})
-
-
+});
 
 export default function Create() {
-
-  const classes = useStyles()
-  const history = useHistory()
-  const [title, setTitle] = useState('')
-  const [details, setDetails] = useState('')
-  const [titleError, setTitleError] = useState(false)
-  const [detailsError, setDetailsError] = useState(false)
-  const [category, setCategory] = useState('money')
+  const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("money");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setTitleError(false)
-    setDetailsError(false)
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
 
-    if(title == ''){
-      setTitleError(true)
+    if (title == "") {
+      setTitleError(true);
     }
-    if(details == ''){
-      setDetailsError(true)
+    if (details == "") {
+      setDetailsError(true);
     }
-    if(title && details){
-      fetch('http://localhost:8000/notes', {
-        method: 'POST',
-        headers: {"Content-type": "application/json"},
-        body: JSON.stringify({title, details, category})
-      }).then(()=> history.push('/'))
+    if (title && details) {
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.push("/"));
     }
-  }
-
+  };
 
   return (
-    <Container size="sm" >
+    <Container size="sm">
       <Typography
         // className={classes.title}
-        variant="h6" 
+        variant="h6"
         component="h2"
         color="textSecondary"
         gutterBottom
       >
         Create a New Note
       </Typography>
-      
-      <form noValidate autoComplete="off" onSubmit={handleSubmit} >
-        <TextField 
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
           onChange={(e) => setTitle(e.target.value)}
           className={classes.field}
           label="Note Title"
@@ -87,7 +83,7 @@ export default function Create() {
           required
           error={titleError}
         />
-        <TextField 
+        <TextField
           onChange={(e) => setDetails(e.target.value)}
           className={classes.field}
           label="Details"
@@ -99,17 +95,23 @@ export default function Create() {
           required
           error={detailsError}
         />
-        
-        <FormControl className={classes.field} >
+
+        <FormControl className={classes.field}>
           <FormLabel>Note Category</FormLabel>
-          <RadioGroup value={category} onChange={(e)=> setCategory(e.target.value)} >
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <FormControlLabel value="money" control={<Radio />} label="Money" />
             <FormControlLabel value="todos" control={<Radio />} label="Todos" />
-            <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
             <FormControlLabel value="work" control={<Radio />} label="Work" />
           </RadioGroup>
         </FormControl>
-
 
         <Button
           // className= {classes.btn}
@@ -121,10 +123,6 @@ export default function Create() {
           Submit
         </Button>
       </form>
-
-      
-
-
 
       {/* <br />
       <AcUnitOutlined />
@@ -142,5 +140,5 @@ export default function Create() {
         <Button>Three</Button>
       </ButtonGroup> */}
     </Container>
-  )
+  );
 }
